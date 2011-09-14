42c85f68c8c6c713ccb17174e838cf8f609b5c85
Yule <paul@dryule.com>
2011-09-14 10:11:39 +0100

    adding mailing tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  user = User.new(:name=>'foobar', :email=>'test@testemail.com',:password=>'test1', :password_confirmation=>'test1')
+  admin = 'test@railsbox.com'
+  
+  it 'should send activate email' do
+    mail = UserMailer.user_activated(user)
+    mail.to.should == [user.email]
+  end
+
+  it 'should send register email' do
+    mail = UserMailer.user_registered(user,admin)
+    mail.to.should == [admin]
+  end
+
+
 end
